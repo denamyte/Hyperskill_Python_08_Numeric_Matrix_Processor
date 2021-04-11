c13f2330959c3a5c8a531f6e1018585a399168d9
denamyte <denamyte@gmail.com>
2021-04-11 03:36:53 +0300

    2021-04-11: Stage 3/6: done

--- Numeric Matrix Processor/task/processor/mtx_op.py
+++ Numeric Matrix Processor/task/processor/mtx_op.py
@@ -20,3 +20,19 @@ class MtxOp:
     @staticmethod
     def multiply_matrix_by_scalar(m: Mtx2D, scalar: float) -> Mtx2D:
         return [[x * scalar for x in row] for row in m]
+
+    @staticmethod
+    def can_multiply_matrix_by_matrix(m1: Mtx2D, m2: Mtx2D) -> bool:
+        return len(m1) > 0 and len(m1[0]) > 0 and len(m1[0]) == len(m2)
+
+    @staticmethod
+    def multiply_matrix_by_matrix(m1: Mtx2D, m2: Mtx2D) -> Mtx2D:
+        m2_dr = MtxOp.main_diagonal_reflection(m2)
+        # a row of m1 gets multiplied by every row of m2_dr, the result is a new matrix row
+        return [[sum(v1 * v2 for v1, v2 in zip(m2_dr[m2_row_ind], m1_row)) for m2_row_ind in range(len(m2_dr))]
+                for m1_row in m1]
+
+    @staticmethod
+    def main_diagonal_reflection(m: Mtx2D) -> Mtx2D:
+        return [[m[row_i][col_i] for row_i in range(len(m))] for col_i in range(len(m[0]))]
+
--- Numeric Matrix Processor/task/processor/user_io.py
+++ Numeric Matrix Processor/task/processor/user_io.py
@@ -1,9 +1,10 @@
-from typing import List, Dict, Callable
+from typing import List, Dict, Callable, Tuple
 from mtx_op import MtxOp, Mtx2D
 
 
 class UserIO:
     """ Input different matrix data and output the results of matrix operations """
+    err_msg = 'The operation cannot be performed.'
 
     def __init__(self):
         self.result: str = ''
@@ -21,7 +22,7 @@ class UserIO:
             if self.result:
                 print(self.result, end='\n\n')
             choice = self.input_choice()
-            self.fun_dic.get(choice, lambda: print('Error!'))()
+            self.fun_dic.get(choice, lambda: print(UserIO.err_msg))()
 
     @staticmethod
     def input_choice() -> int:
@@ -33,30 +34,35 @@ class UserIO:
 Your choice: '''))
 
     def add_matrices(self):
-        matrices = self.input_matrix('first '), self.input_matrix('second '),
-        self.result = 'ERROR' if not MtxOp.can_add_matrices(*matrices) \
+        matrices = self.input_2_matrices()
+        self.result = UserIO.err_msg if not MtxOp.can_add_matrices(*matrices) \
             else self.mtx_to_str(MtxOp.sum_of_matrices(*matrices))
 
     def multiply_matrix_by_scalar(self):
-        pass
+        m = self.input_matrix()
+        scalar = self.input_constant()
+        self.result = self.mtx_to_str(MtxOp.multiply_matrix_by_scalar(m, scalar))
 
     def multiply_matrices(self):
-        pass
+        matrices = self.input_2_matrices()
+        self.result = UserIO.err_msg if not MtxOp.can_multiply_matrix_by_matrix(*matrices) \
+            else self.mtx_to_str(MtxOp.multiply_matrix_by_matrix(*matrices))
 
-    def stage2(self):
-        mtx = self.input_matrix()
-        scalar = int(input())
-        self.result = self.mtx_to_str(MtxOp.multiply_matrix_by_scalar(mtx, scalar))
+    def input_2_matrices(self) -> Tuple[Mtx2D, Mtx2D]:
+        return self.input_matrix('first '), self.input_matrix('second '),
 
     def input_matrix(self, mtx_ord='') -> Mtx2D:
-        lines = self.read_int_row(mtx_ord)[0]
+        lines = self.input_int_row(mtx_ord)[0]
         print(f'Enter {mtx_ord}matrix:')
         return [list(map(float, input().split(' '))) for _ in range(lines)]
 
-    def read_int_row(self, mtx_ord='') -> List[int]:
+    def input_int_row(self, mtx_ord='') -> List[int]:
         return list(map(int, input(f'Enter size of {mtx_ord}matrix: ').split(' ')))
 
+    def input_constant(self) -> float:
+        return float(input('Enter constant: '))
+
     @staticmethod
-    def mtx_to_str(m: Mtx2D, num_map_fn=int) -> str:
+    def mtx_to_str(m: Mtx2D, num_map_fn=float) -> str:
         """ Convert a matrix to a string representation using a specific number mapping function """
         return 'The result is:\n' + '\n'.join(' '.join(map(str, map(num_map_fn, row))) for row in m)
